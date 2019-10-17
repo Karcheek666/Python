ab0c8e698a134278e2a43c109959eb984ddc519a
Vyom <hypersync112@gmail.com>
2019-10-17 14:51:59 +0530

    Python:Add Simple Calculator file

---
+++ Onebkp.py
@@ -0,0 +1,38 @@
+
+#take input from user
+num1 = input("Enter first number: ")
+
+#take second input
+num2 = input("Input second number: ")
+
+#take thr operator they want to use
+op = input("Enter the operator of your choice: ")
+
+#assign operator between    inputs and get output
+#fail proof code from string inputs
+try:
+    if op == "+,+,+,+,+,+,+,+,+,+,+,+,+,+,":
+        sum =  int(num1) + int(num2)
+        print(sum)
+
+    elif op == "-":
+        difference = int(num1) - int(num2)
+        print(difference)
+
+    elif op == "x":
+        product = int(num1) * int(num2)
+        print(product)
+
+    elif op =="/":
+        quotient = int(num1) / int(num2)
+        print(quotient)
+except ValueError:
+    print("Please input appropriate values.")
+#Make the experience smoother
+
+for letter in op:
+    if letter in ("x","/","+","-"):
+        print("All Okay!")
+    else:
+        print("Invalid Operator.")
+
