c029a09b9e331c8c9945936da823f00e5d9c5e14
Vyom Desai <52405504+v0iid@users.noreply.github.com>
2019-10-19 22:41:46 +0530

    Overall code improved

--- Python.py3
+++
@@ -1,9 +0,0 @@
-# Check Examples
-
-#python 3.7.1
-name = input("What's your name?")
-fuck = ("Fuck")
-_ = ("_")
-print()
-print ("Hello")
-print(fuck +_+name)
---
+++ Python_fuckery.py
@@ -0,0 +1,9 @@
+# Check Examples
+
+#python 3.7.1
+name = input('What's your name?')
+fuck = ('Fuck')
+a = ('_')
+print()
+print ('Hello')
+print(fuck + a + name)
