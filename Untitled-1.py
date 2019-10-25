4d196e32edd73631fba0b4b6a5f8e5f4a704aaec
Karcheek666 <56692719+Karcheek666@users.noreply.github.com>
2019-10-25 22:25:54 +0530

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,15 @@
-name = input('Whats your name?')
-fuck = ('Fuck')
-a = ('_')
-print()
-print ('Hello')
-print(fuck + a + name)
+import random
+number = input("Number of times to flip coin: ")
+recordList = []
+heads = 0
+tails = 0
+for amount in range(int(number)):
+     flip = random.randint(0, 1)
+     if (flip == 0):
+          print("Heads")
+          recordList.append("Heads")
+     else:
+          print("Tails")
+          recordList.append("Tails")
+print(str(recordList))
+print(str(recordList.count("Heads")) + str(recordList.count("Tails")))
